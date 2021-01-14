a1fffbc03e1831279ccec872e1d889651d0910f0
Carlos Zamorano <carlos.zamorano@gus.global>
2021-01-14 12:21:18 +0100

    update app module

--- issues-of-repo/src/app/app.module.ts
+++ issues-of-repo/src/app/app.module.ts
@@ -1,28 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 import { NgxPaginationModule } from 'ngx-pagination';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormUrlRepoComponent } from './form-url-repo/form-url-repo.component';
-import { ListIssuesComponent } from './list-issues/list-issues.component';
-import { AngularFireModule, FirebaseApp } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
-
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormUrlRepoComponent,
-    ListIssuesComponent,
+    FormUrlRepoComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule,
     ReactiveFormsModule,
+    FormsModule,
     HttpClientModule,
     NgxPaginationModule
   ],
--- issues-of-repo/src/app/form-url-repo/form-url-repo.component.ts
+++ issues-of-repo/src/app/form-url-repo/form-url-repo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IssueInterface } from '../module/issue.interface';
 import { IssueslistService } from '../services/issueslist.service';
 @Component({
